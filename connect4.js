0293f48c526ff18c921454c7d85eb62f5c3ce28d
anela <anela89@gmail.com>
2022-05-26 17:16:14 -0700

    fixed context of _win

--- connect4.js
+++ connect4.js
@@ -115,11 +115,8 @@ class Game {
   /** checkForWin: check board cell-by-cell for "does a win start here?" */
 
   checkForWin() {
-    function _win(cells) {
-      // Check four cells to see if they're all color of current player
-      //  - cells: list of four (y, x) cells
-      //  - returns true if all are legal coordinates & all match currPlayer
-
+    const _win = cells => {
+      console.log("under _win this =", this);
       return cells.every(
         ([y, x]) =>
           y >= 0 &&
@@ -129,6 +126,20 @@ class Game {
           this.board[y][x] === this.currPlayer
       );
     }
+    // function _win(cells) {
+    //   // Check four cells to see if they're all color of current player
+    //   //  - cells: list of four (y, x) cells
+    //   //  - returns true if all are legal coordinates & all match currPlayer
+    //   console.log("under _win this =", this);
+    //   return cells.every(
+    //     ([y, x]) =>
+    //       y >= 0 &&
+    //       y < this.height &&
+    //       x >= 0 &&
+    //       x < this.width &&
+    //       this.board[y][x] === this.currPlayer
+    //   );
+    // }
 
     for (let y = 0; y < this.height; y++) {
       for (let x = 0; x < this.width; x++) {
